2951cfb8a8e6a41c415e7d5183b6843dca9a616a
Unknown <armstrongko@ornl.gov>
2017-01-27 16:57:50 -0500

    So many changes
    - A lot of changes
    - Added tabs
    - Got rid of sidebar
    - Still need for short term: have 2 columns for mold choice so can start comparing

--- ui.R
+++ ui.R
@@ -9,28 +9,35 @@ library(shiny)
 library(readr)
 library(plotly)
 
-shinyUI(fluidPage(
-
-  # Application title
-  titlePanel("CFRP Energy Use Estimation Tool"),
-
-  # Sidebar with a slider input for number of bins
-  sidebarLayout(
-    sidebarPanel(
-    img(scr = "CFRP Scope.png", height = 150),
-    #WHY ISN'T THE PICTURE APPEARING?
-    p("This tool is being developed by ORNL to provide CFRP researchers and manufacturers the ability to quickly estimate the embodied energy use of their CFRP manufactuing process and compare it to other processes")
-    
-      ),
-  
-
-  mainPanel(
-    
-    selectizeInput("moldingInput", label = ("Molding Technology Options"),
-                   choices = NULL,
-                   selected = "",
-                   multiple = FALSE
-    ),
-    h3("Embodied energy of molding technology:"),
-    h3(textOutput("EnergyNum"),"MJ/kg")
-  ))))
+shinyUI(fluidPage(theme = "bootstrap.css",
+                  
+                  # Application title
+                  titlePanel("CFRP Energy Use Estimation Tool"),
+                  
+                  # Sidebar with a slider input for number of bins
+                  sidebarLayout(
+                    sidebarPanel( width = 0),
+                    
+                    
+                    mainPanel(
+                      tabsetPanel(
+                        tabPanel("Guide",h1("Tool Guide"),img(src = "CFRPScope.png", height = 300),p(("This tool is being developed by ORNL to provide CFRP researchers and manufacturers the ability to quickly estimate the embodied energy use of their CFRP manufactuing process and compare it to other processes"))),
+                        tabPanel("Initial Inputs", h1("Initial"), p("This is where users will input information like part name, part weight, and molding technology to be used")),
+                        tabPanel("Fiber", h1("Fiber Manufacturing"), p("This is where users will choose fiber type, tow and intermediates and set fiber fraction and layup scrap rate")),
+                        tabPanel("Matrix", h1("Matrix Materials Manufacturing"), 
+                                 p("This is where users will choose the primary matrix material and other materials (additional matrix, additives, fillers and inserts) and the mass fraction of each")),
+                        
+                        
+                        tabPanel("Molding", h1("Molding, Curing & Finishing"),
+                                 p("This is where users will choose curing technology, amount of finishing, finishing scrap rate and confirm the molding technology and molding yield"),
+                                 selectizeInput("moldingInput", label = (h3("Molding Technology Options")),
+                                                choices = NULL,
+                                                selected = "",
+                                                multiple = FALSE   ),
+                                 h3("Embodied energy:", textOutput("EnergyNum"),"MJ/kg")
+                        ),
+                        tabPanel("Summary", h1("Summary"),
+                                 p("This is where users will be able to review all of the previous choices")),
+                        tabPanel("Results", h1("Results"),
+                                 p("This is where users will be able to graphically compare the two technology pathways"))
+                      )))))
