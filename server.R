52723686e9c24f0a97bf88f94cc2f34634512d5e
Kristina Armstrong <armstrongko@ornl.gov>
2017-01-30 17:18:37 -0500

    Continued Building
    - Too much to name (started fiber, made columns under initial, etc)
    - added math.R to put math in at a later date

--- server.R
+++ server.R
@@ -8,20 +8,84 @@
 library(shiny)
 library(readr)
 library(plotly)
+source("math.R")
 
 Data_Mold = read_csv("data/Data_Mold.csv")
 moldnames = Data_Mold$Name_Mold
 moldenergy = Data_Mold$Energy_Mold
+moldfrac = Data_Mold$Frac_Fiber
+moldyield = Data_Mold$Yield_Mold
 
 shinyServer(function(input, output, session) {
   
+  output$partname1 <- renderText({paste("Part Name:",input$name1)})
+  output$partweight1 <- renderText({paste("Part Weight:",input$finalweight1, "kg")})
+  output$partname2 <- renderText({paste("Part Name:",input$name2)})
+  output$partweight2 <- renderText({paste("Part Weight:",input$finalweight2, "kg")})
   
+  # Mold1 ----
+  # Make List for Box
   updateSelectizeInput(session, 'moldingInput1',
                     choices = moldnames,
                     selected = "",
                     server = TRUE)
-  moldenergyvalue <- eventReactive(input$moldingInput1, {
+ # Associate Name value with mold type name
+  moldnamevalue1 <- eventReactive(input$moldingInput1, {
+    moldnames[moldnames %in% input$moldingInput1]
+  })
+  output$moldname1 <- renderText(moldnamevalue1())
+  
+   # Associate Energy value with mold type name
+  moldenergyvalue1 <- eventReactive(input$moldingInput1, {
     moldenergy[moldnames %in% input$moldingInput1]
   })
-  output$EnergyNum <- renderText(moldenergyvalue())
-})
+  output$EnergyNum1 <- renderText(moldenergyvalue1())
+
+  # Associate Fiber Frac value with mold type name
+   moldfracvalue1 <- eventReactive(input$moldingInput1,{
+    moldfrac[moldnames %in% input$moldingInput1]
+  })
+  output$moldfracNum1 <- renderText(moldfracvalue1())
+  
+    # Associate Layup yield with mold type name
+  moldyieldvalue1 <- eventReactive(input$moldingInput1,{
+    moldyield[moldnames %in% input$moldingInput1]
+  })
+  output$moldyieldNum1 <- renderText(moldyieldvalue1())
+  
+  # Mold2 ----
+  updateSelectizeInput(session, 'moldingInput2',
+                       choices = moldnames,
+                       selected = "",
+                       server = TRUE)
+  
+  # Associate Name value with mold type name
+  moldnamevalue2 <- eventReactive(input$moldingInput2, {
+    moldnames[moldnames %in% input$moldingInput2]
+  })
+  output$moldname2 <- renderText(moldnamevalue2())
+  
+  moldenergyvalue2 <- eventReactive(input$moldingInput2, {
+    moldenergy[moldnames %in% input$moldingInput2]
+  })
+  output$EnergyNum2 <- renderText(moldenergyvalue2())
+  # Associate Fiber Frac value with mold type name
+  moldfracvalue2 <- eventReactive(input$moldingInput2,{
+    moldfrac[moldnames %in% input$moldingInput2]
+  })
+  output$moldfracNum2 <- renderText(moldfracvalue2())
+  
+  # Associate Layup yield with mold type name
+  moldyieldvalue2 <- eventReactive(input$moldingInput2,{
+    moldyield[moldnames %in% input$moldingInput2]
+  })
+  output$moldyieldNum2 <- renderText(moldyieldvalue2())
+ 
+
+  
+  
+  
+  
+  
+  
+  })
--- ui.R
+++ ui.R
@@ -14,31 +14,31 @@ shinyUI(fluidPage(theme = "bootstrap.css",
                   # Application title
                   titlePanel("CFRP Energy Use Estimation Tool"),
                   
-                  # Sidebar with a slider input for number of bins
                   sidebarLayout(
                     sidebarPanel( width = 0),
                     
                     
                     mainPanel(
-                      tabsetPanel(
+                      tabsetPanel(  
+                      # GuideTab ----
                         tabPanel("Guide",h1("Tool Guide"),img(src = "CFRPScope.png", height = 300),p(("This tool is 
                             being developed by ORNL to provide CFRP researchers and manufacturers the ability 
                             to quickly estimate the embodied energy use of their CFRP manufactuing process 
                             and compare it to other processes"))),
                       
-                          
-                      tabPanel("Initial Inputs", h1("Initial"), p("This is where users will input information 
-                                  like part name, part weight, and molding technology to be used"),
+                      # InitialTab ----   
+                      tabPanel("Initial", h1("Initial Inputs"), p("Enter information on modeled part: 
+                                 part name, part weight, and molding technology to be used"),
                         fluidRow(
                           column(6,
                                h3("Technology Set 1"),
-                               textInput("name1", "Part Name",""),
+                               textInput("name1", "Part Name","Part 1"),
                                numericInput("finalweight1","Final Part Weight (kg)", 1,  min = 0,NA,NA)
                                
                            ),
                            column(6,
                                h3("Technology Set 2"),
-                               textInput("name2", "Part Name",""),
+                               textInput("name2", "Part Name","Part 2"),
                                numericInput("finalweight2","Final Part Weight (kg)", 1, min = 0,NA,NA)
                            )
                         ),
@@ -51,25 +51,70 @@ shinyUI(fluidPage(theme = "bootstrap.css",
                                        multiple = FALSE,
                                        options = list(placeholder = 'Choose Molding Technology')),
                         h3("Embodied energy:"),
-                        fluidRow(column (4,wellPanel(h4(textOutput("EnergyNum")))), column(2, offset = 1,h4("MJ/kg")))
+                        fluidRow(column (3,wellPanel(h4(textOutput("EnergyNum1")))), column(2, offset = 1,h4("MJ/kg")))
+                        ),
+                        column(6,
+                               selectizeInput("moldingInput2", label = "",
+                                              choices = NULL,
+                                              selected = "",
+                                              multiple = FALSE,
+                                              options = list(placeholder = 'Choose Molding Technology')),
+                               h3("Embodied energy:"),
+                               fluidRow(column (3,wellPanel(h4(textOutput("EnergyNum2")))), column(2, offset = 1,h4("MJ/kg")))
                         )
                        
                         )),
                       
+                      # FiberTab ----
+                        tabPanel("Fiber", h1("Fiber Manufacturing & Layup") 
+                       , p("Enter information on modeled part: fiber type, 
+                          tow and intermediates, and fiber fraction and layup scrap rate")
                         
-                        tabPanel("Fiber", h1("Fiber Manufacturing"), p("This is where users will choose fiber type, 
-                          tow and intermediates and set fiber fraction and layup scrap rate")),
-                        tabPanel("Matrix", h1("Matrix Materials Manufacturing"), 
+                      #Display Part Name & Weight & Molding Process
+                      , fluidRow( 
+                      column(6,
+                            h3("Technology Set 1")
+                       ,    h4(textOutput("partname1"), textOutput("partweight1"))
+                       ,    h5(div("Molding Techology: ", textOutput("moldname1")))
+
+                      #Display fiber fraction associated with molding process
+                      ,    h5(div("Default Fiber Fraction: ",textOutput("moldfracNum1")))
+                       ),
+                      #Place to enter Mass fraction?
+                      
+                      #select tow size (don't really need fiber type right now... ask sujit)
+                      #select intermediate
+                      #display default scrap
+                      #enter user scrap
+                      #enter recycle
+                      column(6,
+                            h3("Technology Set 2")
+                       ,    h4(textOutput("partname2"), textOutput("partweight2"))
+                       ,    h5(div("Molding Techology: ", textOutput("moldname2")))
+
+                             #Display fiber fraction associated with molding process
+                       ,    h5(div("Default Fiber Fraction: ",textOutput("moldfracNum2")))
+                      )                       
+                       
+                      )),
+                      
+                      
+                      # MatrixTab ----
+                      tabPanel("Matrix", h1("Matrix Materials Manufacturing"), 
                                  p("This is where users will choose the primary matrix material and other materials 
                                    (additional matrix, additives, fillers and inserts) and the mass fraction of each")),
                         
-                        
+                      # MoldingTab ----  
                         tabPanel("Molding", h1("Molding, Curing & Finishing"),
                                  p("This is where users will choose curing technology, amount of finishing, 
                                    finishing scrap rate and confirm the molding technology and molding yield")
                         ),
+                      # SummaryTab ----
                         tabPanel("Summary", h1("Summary"),
                                  p("This is where users will be able to review all of the previous choices")),
-                        tabPanel("Results", h1("Results"),
+                      # ResultsTab ---- 
+                       tabPanel("Results", h1("Results"),
                                  p("This is where users will be able to graphically compare the two technology pathways"))
+                      
+                      # End ----
                       )))))
