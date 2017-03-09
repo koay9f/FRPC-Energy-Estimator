d4283f557480bd163ac39385aec82beb9fd85b14
Alex Pawlowski <apawlows@vols.utk.edu>
2017-03-09 15:14:01 -0500

    Fixed it for real this time to scale for mobile.

--- ui.R
+++ ui.R
@@ -33,7 +33,7 @@ shinyUI(fluidPage(title = "CFRP Tool",
             
             # GuideTab ----
             tabPanel("Guide",h1("Tool Guide"),
-                     tags$object(type='image/svg+xml', data='ToolScope.svg', id='diagram', height = "300")
+                     tags$object(type='image/svg+xml', data='ToolScope.svg', style = "height: 300; width: 100%; max-width: 778.3079951422452px;")
                      , br()
                      , hr()
                      , br()
